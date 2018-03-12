3ba4045b47b144a37d2bca4a2954dfc6d6dddaee
Ted <tedsimonian@gmail.com>
2018-03-12 12:07:44 -0700

    Deleteing extra files

--- project/resources/views/emails/appointmentPaidEmployee.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-# Hi {{$employee->first_name}} {{$employee->last_name}},
-
-User {{$user->first_name}} {{$user->last_name}} has successfully created and paid for an appointment at {{$event['day']}} {{$event['timeslot']}}.
-
-Thanks,<br>
-
-{{ config('app.name') }}
-@endcomponent
